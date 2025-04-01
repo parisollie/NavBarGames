1ef09c9b648b181285cdf22713a8f0670e819967
parisollie <pjff18@gmail.com>
2025-03-31 20:50:14 -0600

    Renovado

--- NavBarGames/NavBarGamesApp.swift
+++ NavBarGames/NavBarGamesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NavBarGamesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
             SplashScreenView()
         }
     }
